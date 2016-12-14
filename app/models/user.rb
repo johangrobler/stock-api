71bbfb4abb77912a54e32a1d363a736260c56c1f
Johan Grobler <johan.grobler@gmail.com>
2016-12-14 23:20:42 +0200

    Default Push

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,14 @@
-class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+class User < ApplicationRecord 
+
+
+  before_create :generate_token
+
+  protected
+
+  def generate_token
+    self.token = loop do
+      random_token = SecureRandom.urlsafe_base64(nil, false)
+      break random_token unless ModelName.exists?(token: random_token)
+    end
+  end
 end
