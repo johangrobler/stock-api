05a61059052fe9fbd9b7eb98126e495c0682fe89
Johan Grobler <johan.grobler@gmail.com>
2016-12-14 23:09:04 +0200

    Default Push

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -4,7 +4,7 @@ module Api
 
 
     include ActionView::Helpers::DateHelper
-    skip_before_action  :verify_authenticity_token
+    skip_before_filter  :verify_authenticity_token
     respond_to :json
  
   
