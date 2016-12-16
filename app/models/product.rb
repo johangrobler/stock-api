90edd4165934ce5da7b44e9c789bf17b27b90958
Johan Grobler <johan.grobler@gmail.com>
2016-12-16 18:38:20 +0200

    Default Push

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,15 +1,25 @@
 class Product < ApplicationRecord
 
+	# relationships
+
 	has_many :stocks
 	has_many :stock_takes
 
 
+	# validation rules
+
 	validates :name, presence: true 
 	validates :barcode, presence: true
 	validates :replenish_quantity, presence: true
-
   	validates :barcode, uniqueness: true
   	validates :name, uniqueness: true
+
+
+
+	# business rules
+	
+	# 1. Add product to all clinics as stock
+	# 2. Set stock replenish quantity 
 
 	after_create :create_stock
 	after_update :create_stock
@@ -18,11 +28,9 @@ class Product < ApplicationRecord
  
 
 		Clinic.all.each do |clinic|
-
 			s = Stock.find_or_create_by(product:self,clinic:clinic)
 			s.replenisch_at_quantity = self.replenish_quantity 
 			s.save
-
 		end
 	end
 
