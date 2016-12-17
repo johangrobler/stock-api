8b540925cd2d8cbdf2df6035b76edd3f512d7542
Johan Grobler <johan.grobler@gmail.com>
2016-12-17 16:31:25 +0200

    Default Push

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -8,6 +8,9 @@ class Stock < ApplicationRecord
   def update_stock_quantity(quantity)
   	self.update_columns({quantity:quantity}) 
   	self.clinic.notify_contact_with_sms(self) if self.quantity <= self.replenisch_at_quantity   
+
+  	push_stock_take
+  	
   end
 
   def push_stock_take
