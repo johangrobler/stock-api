031f1d49f1d87414db61a8ab4dfc1ccaf1ae679f
Johan Grobler <johan.grobler@gmail.com>
2016-12-17 16:40:47 +0200

    Default Push

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -9,28 +9,9 @@ class Stock < ApplicationRecord
   	self.update_columns({quantity:quantity}) 
   	self.clinic.notify_contact_with_sms(self) if self.quantity <= self.replenisch_at_quantity   
 
-  	push_stock_take
-  	
-  end
-
-  def push_stock_take
-  	
-  	require 'pusher'
-    pusher_client = Pusher::Client.new(
-        app_id: '226476',
-        key: '62217549218f218bf085',
-        secret: '3b2d56477030f12e48df',
-        cluster: 'eu',
-        encrypted: true
-    )
-  
- 
-    pusher_client.trigger("mezzanine-stocktake", 'stocktake_event', {
-              stock: self
-    })
-  
- 
+  	 
 
   end
+
 
 end
--- app/models/stock_take.rb
+++ app/models/stock_take.rb
@@ -1,4 +1,32 @@
 class StockTake < ApplicationRecord
   belongs_to :product
   belongs_to :clinic
+
+
+  after_create :push_stock_take
+
+
+  def push_stock_take
+  	
+ 	puts "push"
+
+  	require 'pusher'
+    pusher_client = Pusher::Client.new(
+        app_id: '226476',
+        key: '62217549218f218bf085',
+        secret: '3b2d56477030f12e48df',
+        cluster: 'eu',
+        encrypted: true
+    )
+  
+ 
+    pusher_client.trigger("mezzanine-stocktake", 'stocktake_event', {
+              stock_take: self
+    })
+  
+ 	puts "pushed"
+
+  end
+
+
 end
